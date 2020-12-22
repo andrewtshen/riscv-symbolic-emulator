5c8ec9e1d008e6d83a2d169a4d8593743579b672
Andrew Shen <33906594+AndrewTShen@users.noreply.github.com>
2020-12-22 17:18:09 -0500

    Updated report example

--- report/test.rkt
+++ report/test.rkt
@@ -1,44 +1,40 @@
 #lang rosette/safe
 
-(require syntax/parse/define)
-
-(define-simple-macro (fresh-symbolic name type)
-  (let () (define-symbolic* name type) name))
-
 (define (uf-memory-write! mem addr val)
   (lambda (addr*)
-    (if (equal? addr addr*)
+    (if (bveq addr addr*)
       val
       (mem addr*))))
 
 (define (uf-memory-read mem addr)
   (mem addr))
 
-(define (array-memory-write! mem addr val)
-  (vector-set! mem addr val))
+(define (array-memory-write! mem addr value)
+  (vector-set! mem addr value))
 
 (define (array-memory-read mem addr)
   (vector-ref mem addr))
 
-(define array_memory (vector 0 1 2))
-(define-symbolic* idx integer?)
-(define-symbolic* pos val integer?)
+(require syntax/parse/define)
+(require (only-in racket/base build-vector))
 
-(printf "Before Write: ~v~n" (array-memory-read array_memory idx))
-(array-memory-write! array_memory pos val)
-(printf "After Write: ~v~n" (array-memory-read array_memory idx))
+(define-simple-macro (make-sym-vector n:expr size:expr m:id)
+  (build-vector n (lambda (i) (define-symbolic* m (bitvector size)) m)))
 
-; >>> Before Write: (ite* (⊢ (= 0 idx$0) 0) (⊢ (= 1 idx$0) 1) (⊢ (= 2 idx$0) 2))
-; >>> After Write: (ite* (⊢ (= 0 idx$0) (ite (= 0 pos$0) val$0 0)) (⊢ (= 1 idx$0) (ite (= 1 pos$0) val$0 1)) (⊢ (= 2 idx$0) (ite (= 2 pos$0) val$0 2)))
+(define array_mem (make-sym-vector 4 8 mem))
 
-(define uf_memory (fresh-symbolic fnmem (~> integer? integer?)))
-(uf-memory-write! uf_memory 0 0)
-(uf-memory-write! uf_memory 1 1)
-(uf-memory-write! uf_memory 2 2)
+(define-symbolic* pos idx (bitvector 2))
+(define-symbolic* val (bitvector 8))
 
-(printf "Before Write: ~v~n" (uf-memory-read uf_memory idx))
-(uf-memory-write! uf_memory pos val)
-(printf "After Write: ~v~n" (uf-memory-read uf_memory idx))
+(printf "Before Write: ~v~n" (array-memory-read array_mem (bitvector->integer idx)))
+(array-memory-write! array_mem (bitvector->integer pos) val)
+(printf "After Write: ~v~n" (array-memory-read array_mem (bitvector->integer idx)))
 
-; >>> Before Write: (app fnmem$0 idx$0)
-; >>> After Write: (app fnmem$0 idx$0)
+(define-simple-macro (fresh-symbolic name type)
+  (let () (define-symbolic* name type) name))
+
+(define uf_mem (fresh-symbolic fnmem (~> (bitvector 2) (bitvector 8))))
+
+(printf "Before Write: ~v~n" (uf-memory-read uf_mem idx))
+(define uf_mem* (uf-memory-write! uf_mem pos val))
+(printf "After Write: ~v~n" (uf-memory-read uf_mem* idx))
