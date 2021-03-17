c66ce229ce5e93a4a7f529961aa47ed38852c7bd
Andrew Shen <33906594+AndrewTShen@users.noreply.github.com>
2021-03-17 00:06:33 -0400

    Added back other tests

--- emulator/test.rkt
+++ emulator/test.rkt
@@ -627,12 +627,12 @@
                    (assert-mem-equal m m1 sym-idx))))
              (check-true (unsat? model_noninterference))))
 
-; (define res-instruction-check (run-tests instruction-check))
-; (define res-utils (run-tests utils))
+(define res-instruction-check (run-tests instruction-check))
+(define res-utils (run-tests utils))
 (define res-high-level-test (run-tests high-level-test))
-; (define res-step-checks (run-tests step-checks))
+(define res-step-checks (run-tests step-checks))
 
-; ;; Testing the base case and inductive step
+;; Testing the base case and inductive step
 
-; (define res-boot-sequence (time (run-tests boot-sequence)))
-; (define res-inductive-step (time (run-tests inductive-step)))
+(define res-boot-sequence (time (run-tests boot-sequence)))
+(define res-inductive-step (time (run-tests inductive-step)))
